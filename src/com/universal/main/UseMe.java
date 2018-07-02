ba46b32bff7f0253f04b5fc4137605924f76d6f8
Lenovo <Lenovo@LAPTOP-FML6P63B>
2018-07-02 18:23:21 +0530

    Merge origin/master
    Conflicts:
    	src/com/universal/main/UseMe.java

--- src/com/universal/main/UseMe.java
+++ src/com/universal/main/UseMe.java
@@ -6,7 +6,6 @@ import com.universal.view.BookListView;
 public class UseMe {
   
     public static void main(String[] args) {
-        System.out.println("vggjghkbk");
 String test="ankit";
         new BookListView().setVisible(true);
           
